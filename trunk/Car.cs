772e7441ed3dc2c60b23b37dce8ff5f96f5956fd
cwollnik <cwollnik@0576be88-ab58-11dd-ac80-e787e3ad770f>
2008-11-20 20:05:23 +0000

    Updated car and input car is now useable input contains the method-skeleton

--- trunk/Car.cs
+++ trunk/Car.cs
@@ -3,9 +3,8 @@
 // Path: \ACT2009\Car
 // Author: Claus Wollnik, Team 2
 // Used to handle the cardata in one place
-// Code lines: 225
 // Creation date: 19.11.2008 19:06
-// Last modified: 11.05.2008 13:15
+// Last modified: 20.11.2008 20:15
 
 
 using System;
@@ -20,33 +19,50 @@ namespace ACT2009
     class Car
     {
         //absolute position the car is at (in meters)
-        Vector3 position;
+        Vector3 position = new Vector3(0);
         //direction the car is looking at
-        Vector3 direction;
+        Vector3 direction = new Vector3(0);
         //velocity the car is currently moving at (kilometers per hour)
-        float speed;
+        float speed = 0;
         //color the car is painted in
-        Color color;
+        Color color = Color.Blue;
         //maximum velocity moving forward (kilometers per hour)
-        float maxSpeedFwd;
+        float maxSpeedFwd = 50;
         //maximum velocity moving backward (kilometers per hour)
-        float maxSpeedRew;
+        float maxSpeedRew = 15;
         //weight of the car (kilogram)
-        float weight;
+        float weight = 300;
         //controller the car listens to
-        Input controller;
+        Input controller = null;
 
         //initializes the car's data
         Car(Input contr)
         {
-            position = new Vector3(0);
-            direction = new Vector3(0);
-            speed = 0;
-            color = Color.Blue;
-            maxSpeedFwd = 50;
-            maxSpeedRew = 15;
-            weight = 300;
+            //can only be set here
             controller = contr;
+        }
+
+        //initializes the car's data
+        Car(Input contr, Color clr)
+        {
+            //can only be set here
+            color = clr;
+
+            //can only be set here
+            controller = contr;
+        }
+
+        //initializes the car's data
+        Car(Input contr, Color clr, float wgt)
+        {
+            //can only be set here
+            color = clr;
+
+            //can only be set here
+            controller = contr;
+
+            //can only be set here
+            weight = wgt;
         }
 
         //returns the current absolute position of the car
@@ -71,6 +87,60 @@ namespace ACT2009
         public void SetDirection(Vector3 pos)
         {
             position = pos;
+        }
+
+        //returns the current speed of the car in km/h
+        public float GetSpeed()
+        {
+            return speed;
+        }
+
+        //sets the current speed of the car in km/h
+        public void SetDirection(float spd)
+        {
+            speed = spd;
+        }
+
+        //returns the color of the car
+        public Color GetColor()
+        {
+            return color;
+        }
+
+        //returns the maximum forward velocity of the car in km/h
+        public float GetMaxSpeedFwd()
+        {
+            return maxSpeedFwd;
+        }
+
+        //sets the maximum forward velocity of the car in km/h
+        public void SetMaxSpeedFwd(float spd)
+        {
+            maxSpeedFwd = spd;
+        }
+
+        //returns the maximum reverse velocity of the car in km/h
+        public float GetMaxSpeedRew()
+        {
+            return maxSpeedRew;
+        }
+
+        //sets the maximum reverse velocity of the car in km/h
+        public void SetMaxSpeedRew(float spd)
+        {
+            maxSpeedRew = spd;
+        }
+
+        //returns the weight of the car in kilogram
+        public float GetWeight()
+        {
+            return weight;
+        }
+
+        //returns the controller the car listens to
+        public Input GetController()
+        {
+            return controller;
         }
     }
 }
--- trunk/Input.cs
+++ trunk/Input.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿// Project: ACT2009, File: Input.cs
+// Namespace: ACT2009, Class: Input
+// Path: \ACT2009\Input
+// Author: Claus Wollnik, Team 2
+// Used to ease the request of carcontrols (needs to be completed, only skeleton)
+// Creation date: 19.11.2008 19:10
+// Last modified: 20.11.2008 20:53
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
 
 namespace ACT2009
 {
     class Input
     {
+        //constructor is subject to change
+        Input()
+        {
+        }
+
+        //returns accelleration between -1 (full reverse) and 1 (full forward)
+        public float GetAccelleration()
+        {
+            return 0;
+        }
+
+        //returns braking between 0 (none) and 1 (full)
+        public float GetBrake()
+        {
+            return 0;
+        }
+
+        //returns accelleration between -1 (left) and 1 (right)
+        public float GetDirection()
+        {
+            return 0;
+        }
+
+        //returns pause status false (play) and true (game paused)
+        public bool GetPause()
+        {
+            return false;
+        }
     }
 }
