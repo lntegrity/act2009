54fe73d77901ec8e88c90d836faf3f72834a5dd8
tobias.feigel@web.de <tobias.feigel@web.de@0576be88-ab58-11dd-ac80-e787e3ad770f>
2008-12-02 15:59:18 +0000

    Added getAngle Method

--- Math.cs
+++ Math.cs
@@ -33,6 +33,26 @@ namespace ACT2009
             return kmh;
         }
 
+        /// <summary>
+        /// Berechnet den Winkel zwischen zwei Vektoren
+        /// </summary>
+        /// <param name="vector1">Erster Vektor</param>
+        /// <param name="vector2">Zweiter Vektor</param>
+        /// <returns>Winkel in Winkelgrad</returns>
+        public static double getAngle(Vector3 vector1, Vector3 vector2)
+        {
+            double angle;
+            double x1;
+            double x2;
+            double x3;
 
+            x1 = Vector3.Dot(vector1, vector2);
+            x2 = System.Math.Sqrt(System.Math.Pow(vector1.X, 2) + System.Math.Pow(vector1.Y, 2) + System.Math.Pow(vector1.Z, 2));
+            x3 = System.Math.Sqrt(System.Math.Pow(vector2.X, 2) + System.Math.Pow(vector2.Y, 2) + System.Math.Pow(vector2.Z, 2));
+
+            angle = System.Math.Acos(x1 / (x2 * x3));
+            angle = angle * 180 / System.Math.PI;
+            return angle;
+        }
     }
 }
