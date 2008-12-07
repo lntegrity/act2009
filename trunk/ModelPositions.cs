12b10eb800c01027ed66ec6569d8e6cb35658d02
c.wollnik@web.de <c.wollnik@web.de@0576be88-ab58-11dd-ac80-e787e3ad770f>
2008-12-07 22:58:45 +0000

    Added ModelPositions.cs to add objects to our world (trees, traffic safety cones, parking cars etc) Edited Display.cs to reuse the draw of a model

---
+++ trunk/ModelPositions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace ACT2009
+{
+    class ModelPositions
+    {
+        //model of the objects to display
+        private Model objectModel;
+        //list of positions of the object
+        private List<Vector3> positions;
+        //list of directions of the object
+        private List<Vector3> directions;
+
+        ModelPositions(Model objectModel,List<Vector3> positions)
+        {
+            this.objectModel = objectModel;
+            this.positions = positions;
+            this.directions = new List<Vector3>(positions.Count);
+
+            for (int i = 0; i < directions.Count; ++i)
+            {
+                directions[i] = Vector3.Zero;
+            }
+        }
+        ModelPositions(Model objectModel,List<Vector3> positions,List<Vector3> directions)
+        {
+            this.objectModel = objectModel;
+            this.positions = positions;
+            this.directions = directions;
+        }
+    }
+}
